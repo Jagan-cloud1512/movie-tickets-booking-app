fe4a648400ca76d78924af8eeefca440b92f7df0
Jagan <jms20041215@gmail.com>
2025-12-29 13:18:03 +0530

    Clean backend deploy

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import movieRoutes from "./routes/movies.js";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use("/api/movies", movieRoutes);
@@ -12,4 +11,4 @@ app.get("/", (req, res) => {
   res.json({ message: "Movie API LIVE!" });
 });
 
-export default app; // ONLY THIS LINE MATTERS FOR VERCEL
+export default app;
