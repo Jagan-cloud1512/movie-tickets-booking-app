d029744f5b14d75345e76104006cb2edc82c41a7
Jagan <jms20041215@gmail.com>
2025-12-29 12:58:01 +0530

    Final deploy fix

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 let currentUser = null;
 // point directly to backend deployment
-const API_BASE = "https://movie-tickets-booking-app-zcrx.vercel.app/api/movies";
+const API_BASE = "https://movie-backend-2.vercel.app/api/movies";
 
 // ---------- AUTH MODAL ----------
 
